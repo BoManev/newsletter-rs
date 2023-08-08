ac8cf4901c82a4ab388003ab3d135f1f0eb633cd
BoManev <bomanev@pm.me>
2023-08-08 04:19:28 -0400

    query for subscriptions

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,15 @@
 use std::net::TcpListener;
 
+use sqlx::PgPool;
 use zero2prod::*;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
     let config = config::get_config().expect("Failed to read configuration!");
+    let conn_pool = PgPool::connect(&config.database.connection_string())
+        .await
+        .expect("Failed to connect to Postgres.");
     let address = format!("127.0.0.1:{}", config.application_port);
     let listener = TcpListener::bind(address).expect("Failed to bind port!");
-    startup::run(listener)?.await
+    startup::run(listener, conn_pool)?.await
 }
