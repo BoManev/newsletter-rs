14f0f289878bf47eb397a9abe2eab3ff7bb6307f
BoManev <bomanev@pm.me>
2023-08-16 20:03:38 -0400

    feat(subscriber): proper error handling

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -167,7 +167,7 @@ async fn subscribe_returns_a_400_when_fields_are_present_but_invalid() {
             .expect("Failed to execute request");
 
         assert_eq!(
-            200,
+            400,
             response.status().as_u16(),
             "Failed to return 400 Bad Request with payload {}",
             desc
