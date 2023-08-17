c72f6c07201ee1b427ffdd36106947f5915ead7f
BoManev <bomanev@pm.me>
2023-08-16 21:16:26 -0400

    feat(subscriber): refactor

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -11,6 +11,12 @@ pub struct FormData {
     name: String,
 }
 
+pub fn parse_subscriber(form: FormData) -> Result<NewSubscriber, String> {
+    let name = SubscriberName::parse(form.name)?;
+    let email = SubscriberEmail::parse(form.email)?;
+    Ok(NewSubscriber { email, name })
+}
+
 // web::Data is a form of dependacy injection (type-map mapping Any to TypeId::of)
 #[tracing::instrument(
     name = "Adding a new subscriber",
@@ -24,16 +30,10 @@ pub async fn subscribe(
     form: web::Form<FormData>,
     pool: web::Data<PgPool>,
 ) -> HttpResponse {
-    let name = match SubscriberName::parse(form.0.name) {
-        Ok(name) => name,
+    let new_subscriber = match parse_subscriber(form.0) {
+        Ok(subscriber) => subscriber,
         Err(_) => return HttpResponse::BadRequest().finish(),
     };
-
-    let email = match SubscriberEmail::parse(form.0.email) {
-        Ok(email) => email,
-        Err(_) => return HttpResponse::BadRequest().finish(),
-    };
-    let new_subscriber = NewSubscriber { email, name };
 
     match insert_subscriber(&pool, &new_subscriber).await {
         Ok(_) => HttpResponse::Ok().finish(),
@@ -67,37 +67,3 @@ pub async fn insert_subscriber(
     })?;
     Ok(())
 }
-
-//     let req_id = Uuid::new_v4();
-//     let req_span = tracing::info_span!(
-//         "Adding a new subscriber",
-//         %req_id,
-//         subscriber_email = %form.email,
-//         subscriber_name = %form.name,
-//     );
-//     let _req_span_guard = req_span.enter();
-
-//     match sqlx::query!(
-//         r#"
-//         INSERT INTO subscriptions (id, email, name, subscribed_at)
-//         VALUES ($1, $2, $3, $4)
-//         "#,
-//         Uuid::new_v4(),
-//         form.email,
-//         form.name,
-//         Utc::now()
-//     )
-//     .execute(pool.as_ref())
-//     .instrument(tracing::info_span!("Saving new subscriber details into db"))
-//     .await
-//     {
-//         Ok(_) => {
-//             tracing::info!("[{req_id}] New subscriber details have been saved");
-//             HttpResponse::Ok().finish()
-//         }
-//         Err(e) => {
-//             tracing::error!("[{req_id}] Failed to execute query: {:?}", e);
-//             HttpResponse::InternalServerError().finish()
-//         }
-//     }
-// }
