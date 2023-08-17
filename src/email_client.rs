20cdea23fd6bc02b9b3c38c936e4576d10a81f88
BoManev <bomanev@pm.me>
2023-08-16 23:37:51 -0400

    feat(email client): wiremock body matcher

--- src/email_client.rs
+++ src/email_client.rs
@@ -9,7 +9,9 @@ pub struct EmailClient {
     auth_token: Secret<String>,
 }
 
+// Use convert between JSON and Rust naming convention
 #[derive(serde::Serialize)]
+#[serde(rename_all = "PascalCase")]
 struct SendEmailRequest {
     from: String,
     to: String,
@@ -51,7 +53,7 @@ impl EmailClient {
             .http_client
             .post(&url)
             .header(
-                "X-Postmark-Server-auth_token",
+                "X-Postmark-Server-Token",
                 self.auth_token.expose_secret(),
             )
             .json(&body)
@@ -70,11 +72,29 @@ mod tests {
     use fake::faker::lorem::en::{Paragraph, Sentence};
     use fake::{Fake, Faker};
     use secrecy::Secret;
-    use wiremock::matchers::any;
+    use wiremock::matchers::{header, header_exists, method, path};
     use wiremock::{Mock, MockServer, ResponseTemplate};
 
+    struct SendEmailBodyMatcher;
+
+    impl wiremock::Match for SendEmailBodyMatcher {
+        fn matches(&self, request: &wiremock::Request) -> bool {
+            let result: Result<serde_json::Value, _> =
+                serde_json::from_slice(&request.body);
+            if let Ok(body) = result {
+                body.get("From").is_some()
+                    && body.get("To").is_some()
+                    && body.get("Subject").is_some()
+                    && body.get("HtmlBody").is_some()
+                    && body.get("TextBody").is_some()
+            } else {
+                false
+            }
+        }
+    }
+
     #[tokio::test]
-    async fn send_email_fires_a_request_to_base_url() {
+    async fn send_email_sends_the_expected_request() {
         let mock_server = MockServer::start().await;
         let sender = SubscriberEmail::parse(SafeEmail().fake()).unwrap();
         let email_client = EmailClient::new(
@@ -83,7 +103,11 @@ mod tests {
             Secret::new(Faker.fake()),
         );
 
-        Mock::given(any())
+        Mock::given(header_exists("X-Postmark-Server-Token"))
+            .and(header("Content-Type", "application/json"))
+            .and(path("/email"))
+            .and(method("POST"))
+            .and(SendEmailBodyMatcher)
             .respond_with(ResponseTemplate::new(200))
             .expect(1)
             .mount(&mock_server)
