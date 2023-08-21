95f35eda03a735411015bc6203bd20d5e30ba17a
BoManev <bomanev@pm.me>
2023-08-20 21:44:00 -0400

    refactor(telemetry): add convinient way to start new background task with attached tracing

--- src/routes/newsletter.rs
+++ src/routes/newsletter.rs
@@ -8,7 +8,10 @@ use reqwest::{header, StatusCode};
 use secrecy::{ExposeSecret, Secret};
 use sqlx::PgPool;
 
-use crate::{domain::SubscriberEmail, email_client::EmailClient};
+use crate::{
+    domain::SubscriberEmail, email_client::EmailClient,
+    telemetry::spawn_blocking_with_tracing,
+};
 
 use super::error_chain_fmt;
 
@@ -189,10 +192,8 @@ async fn validate_credentials(
         .map_err(PublishError::UnexpectedError)?
         .ok_or_else(|| PublishError::AuthError(anyhow::anyhow!("Unknown username")))?;
 
-    let current_span = tracing::Span::current();
-    tokio::task::spawn_blocking(move || {
-        current_span
-            .in_scope(|| verify_password_hash(expected_hash, credentials.password))
+    spawn_blocking_with_tracing(move || {
+        verify_password_hash(expected_hash, credentials.password)
     })
     .await
     .context("Failed to spawn blocking task")
